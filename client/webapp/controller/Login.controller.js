ee182ced56e78ceb7ff5afe2f621c4de8387a4a1
elifintizamoglu <elifintizam@gmail.com>
2024-05-24 09:47:16 +0300

    fixes

--- client/webapp/controller/Login.controller.js
+++ client/webapp/controller/Login.controller.js
@@ -10,143 +10,51 @@ sap.ui.define([
             var oView = this.getView();
             var sUsername = oView.byId("username").getValue();
             var sPassword = oView.byId("password").getValue();
-            var flag = false;
-
-            $.ajax({
-                url: "http://localhost:5000/auth/login",
+            
+            fetch("http://localhost:5000/auth/login", {
                 method: "POST",
-                contentType: "application/json",
-                data: JSON.stringify({ username: sUsername, password: sPassword }),
-                success: function (data) {
-                    MessageToast.show("Login successful!");
-                    // Navigate to the home view
-                    flag = true;
-                }.bind(this), // Ensure the context (this) is correct
-                error: function () {
-                    MessageToast.show("Login failed!");
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username: sUsername, password: sPassword })
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
                 }
+                throw new Error("Login failed!");
+            })
+            .then(data => {
+                MessageToast.show("Login successful!");
+                // var oRouter = UIComponent.getRouterFor(this);
+                // oRouter.navTo("home");
+            })
+            .catch(error => {
+                MessageToast.show(error.message);
             });
-            if(flag){
-                var oRouter = UIComponent.getRouterFor(this);
-                    oRouter.navTo("home");
-            }
         },
         onRegister: function () {
             var oView = this.getView();
             var sUsername = oView.byId("username").getValue();
             var sPassword = oView.byId("password").getValue();
-
-            $.ajax({
-                url: "http://localhost:5000/auth/register",
+            
+            fetch("http://localhost:5000/auth/register", {
                 method: "POST",
-                contentType: "application/json",
-                data: JSON.stringify({ username: sUsername, password: sPassword }),
-                success: function () {
-                    MessageToast.show("User registered!");
+                headers: {
+                    "Content-Type": "application/json"
                 },
-                error: function () {
-                    MessageToast.show("Registration failed!");
+                body: JSON.stringify({ username: sUsername, password: sPassword })
+            })
+            .then(response => {
+                if (response.ok) {
+                    MessageToast.show("User registered!");
+                } else {
+                    throw new Error("Registration failed!");
                 }
+            })
+            .catch(error => {
+                MessageToast.show(error.message);
             });
         }
     });
 });
-
-
-// sap.ui.define([
-//     "sap/ui/core/mvc/Controller",
-//     "sap/m/MessageToast",
-//     "sap/ui/core/UIComponent"
-// ], function (Controller, MessageToast, UIComponent) {
-//     "use strict";
-
-//     return Controller.extend("sap.ui.demo.client.controller.Login", {
-//         onLogin: function () {
-//             var oView = this.getView();
-//             var sUsername = oView.byId("username").getValue();
-//             var sPassword = oView.byId("password").getValue();
-
-//             $.ajax({
-//                 url: "http://localhost:5000/auth/login",
-//                 method: "POST",
-//                 contentType: "application/json",
-//                 data: JSON.stringify({ username: sUsername, password: sPassword }),
-//                 success: function (data) {
-//                     MessageToast.show("Login successful!");
-//                     var oRouter = UIComponent.getRouterFor(this);
-//                     oRouter.navTo("app",{});
-//                 }.bind(this),
-//                 error: function () {
-//                     MessageToast.show("Login failed!");
-//                 }
-//             });
-//         },
-//         onRegister: function () {
-//             var oView = this.getView();
-//             var sUsername = oView.byId("username").getValue();
-//             var sPassword = oView.byId("password").getValue();
-
-//             $.ajax({
-//                 url: "http://localhost:5000/auth/register",
-//                 method: "POST",
-//                 contentType: "application/json",
-//                 data: JSON.stringify({ username: sUsername, password: sPassword }),
-//                 success: function () {
-//                     MessageToast.show("User registered!");
-//                 },
-//                 error: function () {
-//                     MessageToast.show("Registration failed!");
-//                 }
-//             });
-//         }
-//     });
-// });
-
-
-// // sap.ui.define([
-// //     "sap/ui/core/mvc/Controller",
-// //     "sap/m/MessageToast"
-// // ], function (Controller, MessageToast) {
-// //     "use strict";
-
-// //     return Controller.extend("myapp.controller.View1", {
-// //         onLogin: function () {
-// //             var oView = this.getView();
-// //             var sUsername = oView.byId("username").getValue();
-// //             var sPassword = oView.byId("password").getValue();
-
-// //             $.ajax({
-// //                 url: "http://localhost:5000/auth/login",
-// //                 method: "POST",
-// //                 contentType: "application/json",
-// //                 data: JSON.stringify({ username: sUsername, password: sPassword }),
-// //                 success: function (data) {
-// //                     MessageToast.show("Login successful!");
-// //                     var oRouter = UIComponent.getRouterFor(this);
-// //                     oRouter.navTo("home");
-// //                 },
-// //                 error: function () {
-// //                     MessageToast.show("Login failed!");
-// //                 }
-// //             });
-// //         },
-// //         onRegister: function () {
-// //             var oView = this.getView();
-// //             var sUsername = oView.byId("username").getValue();
-// //             var sPassword = oView.byId("password").getValue();
-
-// //             $.ajax({
-// //                 url: "http://localhost:5000/auth/register",
-// //                 method: "POST",
-// //                 contentType: "application/json",
-// //                 data: JSON.stringify({ username: sUsername, password: sPassword }),
-// //                 success: function () {
-// //                     MessageToast.show("User registered!");
-// //                 },
-// //                 error: function () {
-// //                     MessageToast.show("Registration failed!");
-// //                 }
-// //             });
-// //         }
-// //     });
-// // });
